6ddfa81eb67bf735271bac9c83842d16f7c34fda
Freelancer Laju <96712468+freelancer-laju@users.noreply.github.com>
2022-05-10 12:29:40 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// KeyboardEvent Object:
+
+// 1. Keydown - pressing a key, can repeat
+// 2. keypress - (may not supported by some browsers)
+// 3. keyup -
+// Some propertise - key, keycode, code, shiftKey,
+// ctlKey, repeat
+
+const textarea = document.querySelector("textarea");
